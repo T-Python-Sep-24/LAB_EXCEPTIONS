c6bc92fcf139f4501bdd6cf4bfb67d9c2642e3f8
SalehBinHamed <157858533+SalehBinHamed@users.noreply.github.com>
2024-10-07 23:23:26 +0300

    done

---
+++ Lap_exceptions.py
@@ -0,0 +1,15 @@
+def additoin(x, y):
+    try:
+        x = 10
+        y = 20
+        print("Addition:", x +b)
+        print("The operation is successful")
+    except NameError as e:
+        print(e)
+        print("NameError")
+    except Exception as e:
+        print(e)
+
+
+
+additoin(10, 20)
