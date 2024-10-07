0f2953729098e4277c5466edae627a929790dd48
HmzhDubh <meezo.2015@hotmail.com>
2024-10-07 13:30:23 +0300

    handled exceptions

---
+++ exceptionsLab.py
@@ -0,0 +1,15 @@
+def additoin(x, y):
+    try:
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+
+    # except Exception as err:
+    #     print(err.__class__)
+    # it's a NameError
+    except NameError:
+        print("your have incorrect variable name, please edit fix the problem")
+    else:
+        print("The operation is successfully")
+
+additoin(10, 20)
