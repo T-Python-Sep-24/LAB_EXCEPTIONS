e148a3ad6f29f8c5e8dd8fd83e40e1c176015623
Ali <almaghamsiali@gmail.com>
2024-10-08 10:23:56 +0300

    solution got changed'

--- exep.py
+++ exep.py
@@ -1,15 +1,13 @@
 def additoin(x, y):
-    try:
-
         x = 10
         y = 20
-        print("Addition:", x + c)
-    except NameError:
-        print("there is a name error in the program try to fix it!")
-    except Exception as e:
+        print("Addition:", x + b)
+
+try:
+    additoin(10, 20)
+except NameError:
+    print("there is a name error in the program try to fix it!")
+except Exception as e:
         print(e.__class__)
-    else:
-        print("the operation is successful")
-
-
-additoin(10, 20)
+else:
+    print("the operation is successful")
